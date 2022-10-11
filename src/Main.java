994e9de71b19489fe0c81dba5a106f3761362c90
J34nPC24 <jean.cotera@epn.edu.ec>
2022-10-11 11:38:29 -0500

    Trabajo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(obtenerNotacionNPI("(3*4)+(5*7)"));
+
+
+    }
+
+    private static String obtenerNotacionNPI(String operacion) {
+        String notacionNPI = "";
+        String auxiliar = "";
+
+        for (int i = 0; i < operacion.length(); i++) {
+            switch (operacion.charAt(i)) {
+                case '+', '-', '*', '/':
+                    auxiliar += operacion.charAt(i);
+                    break;
+                case '(', '{', '[':
+                    auxiliar += operacion.charAt(i);
+                    break;
+                case ')':
+                    notacionNPI += auxiliar.substring(auxiliar.lastIndexOf('(') + 1);
+                    auxiliar = auxiliar.substring(0, auxiliar.lastIndexOf('('));
+                    break;
+                case '}':
+                    notacionNPI += auxiliar.substring(auxiliar.lastIndexOf('{') + 1);
+                    auxiliar = auxiliar.substring(0, auxiliar.lastIndexOf('{'));
+                    break;
+                case ']':
+                    notacionNPI += auxiliar.substring(auxiliar.lastIndexOf('[') + 1);
+                    auxiliar = auxiliar.substring(0, auxiliar.lastIndexOf('['));
+                    break;
+                case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+                    notacionNPI += operacion.charAt(i);
+                    break;
+                default:
+                    System.out.println("Que carajos ingresaste, no es ni un numero o un signo de agrupacion");
+                    break;
+            }
+        }
+        notacionNPI += auxiliar;
+        return notacionNPI;
     }
 }
